371b69c021b33f6f6487755ad9f0b4c758b61ca2
rmondav <12041885+rmondav@users.noreply.github.com>
2022-10-31 14:39:51 +0100

    update slurm queue script

--- scripts/sbatch_network_calculations.sh
+++ scripts/sbatch_network_calculations.sh
@@ -6,7 +6,7 @@
 #SBATCH -p core -n 4
 
 module load bioinfo-tools R/4.0.4 R_packages/4.0.4
-maindir="/proj/snic2020-16-196/nobackup/CHtrophy_network_nobackup/methanotrophy_networks"
+maindir="/path/to/working/dir"
 cd $maindir/processed_tables/
 Rscript --no-restore --no-save "$maindir/scripts/SPIEC-EASI.R" &&
 cd ..
